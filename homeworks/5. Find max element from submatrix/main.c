52f4d0ae8c1a44dffebb43eed11a6a04370d79e8
Esaú Peralta <esau.opr@gmail.om>
2019-08-14 18:09:58 -0500

    feat(homework): Add homework 5

---
+++ homeworks/5. Find max element from submatrix/main.c
@@ -0,0 +1,30 @@
+/**
+    ANSI C standard: c11
+    Purpose: Solve a square matrix by Gaussian elimination
+
+    @author Esaú Peralta
+    @email esau.opr@gmail.com
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../src/matrix.h"
+#include "../../src/linear_equations_systems_solutions/gaussian_elimination.h"
+
+
+int main(int argc, char const *argv[]) {
+    int size;
+
+    scanf("%d", &size);
+
+    double **matrix = read_matrix(size, size);
+    print_matrix(matrix, size, size);
+
+    matrix_point mp = find_matrix_max_element(matrix, 0, 0, size - 1, size - 1);
+
+    printf("El elemento con mayor valor obsoluto es: %lf, en (%d, %d)\n", mp.value, mp.row + 1, mp.col + 1);
+
+    free_matriz(matrix, size, size);
+
+    return 0;
+}
