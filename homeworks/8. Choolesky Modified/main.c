16bebee958e871557c5711be2f9868ae1aaf51b5
Esau Peralta <oscar.peralta@cimat.mx>
2019-08-23 19:11:12 -0500

    feat(homeworks): Add cholesky method

---
+++ homeworks/8. Choolesky Modified/main.c
@@ -0,0 +1,35 @@
+/**
+    ANSI C standard: c11
+    Purpose: Solve a square matrix by Gaussian elimination
+
+    @author Esaú Peralta
+    @email esau.opr@gmail.com
+*/
+
+
+#include <stdlib.h>
+#include "../../src/linear_equations_systems_solutions/cholesky.h"
+
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        perror("main(): 2 Args missing");
+        exit(EXIT_FAILURE);
+    }
+
+    AugmentedMatrix matrix = read_augmented_matrix(argv[1], argv[2]);
+    print_matrix(matrix.content, matrix.rows, matrix.cols);
+
+    SystemSolution system_solution = solve_by_cholesky_method(matrix);
+
+    if ( system_solution.solution == NULL) {
+        puts("The system has not a unique solution");
+    } else {
+        print_system_solution(system_solution);
+    }
+
+    free_matriz(matrix);
+    free(system_solution.solution);
+
+    return 0;
+}
